fbb9b8a8e8db5750238d1cbcc050e0d6efb0fd83
YJ0422 <24708595l30@gmail.com>
2026-03-03 20:25:29 +0900

    include child components

--- components/ChatBox.js
+++ components/ChatBox.js
@@ -20,10 +20,43 @@ export default function ChatBox() {
             { role: 'user', text : question }
         ]);
 
-        setLoading(true);
+        setLoading(true); // 입력창 disabled 시킴
 
         // 서버 호출 후 질문에 따른 답변 받아오기
+        const res = await fetch('/api', {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({question}) // {question: question} 형태
+        });
 
+        // 응답 body json -> js 객체화
+        const data = await res.json();
 
+        // 챗봇 응답 추가
+        setMessages(prev => [
+            ...prev,
+            // || 는 왼쪽 값이 falsy면 오른쪽 값 사용
+            { role: 'bot', text : data.answer || '오류 발생'}
+        ]);
+
+        setLoading(false);
     }
+
+    return (
+        <div style={{maxWidth: 700, margin: '0 auto'}}>
+            <h2>💭 RAG 챗봇</h2>
+
+            {/* 메시지 창 */}
+            <MessageList 
+                messages={messages} 
+                loading={loading}
+            />
+
+            {/* 인풋 입력창 */}
+            <ChatInput
+                onSend={ask}
+                disabled={loading}
+            />
+        </div>
+    )
 }
